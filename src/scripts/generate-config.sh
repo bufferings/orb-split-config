b5e3eacabc62fa817d79b25f5fd664a7e9e130b4
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-07-21 01:27:14 +0900

    Small improvements

--- src/scripts/generate-config.sh
+++ src/scripts/generate-config.sh
@@ -4,9 +4,13 @@ echo "Config list ==="
 
 cat "${PARAM_CONFIG_LIST_PATH}"
 
+echo
+
 echo "Generated YAML ==="
 
 cat "${PARAM_CONFIG_LIST_PATH}" \
 | awk '{printf "\"%s\" ", $0}' \
 | xargs -0 -I {} sh -c 'cue export {} --out yaml' \
 | tee "${PARAM_GENERATED_CONFIG_PATH}"
+
+echo
