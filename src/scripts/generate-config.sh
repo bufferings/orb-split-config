a28b5642089e80c4fee12e527faa1f7bd7f5b185
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-07-21 23:47:34 +0900

    Fix awk to skip empty line

--- src/scripts/generate-config.sh
+++ src/scripts/generate-config.sh
@@ -8,7 +8,7 @@ echo
 echo "Generated YAML ==="
 
 cat "${PARAM_CONFIG_LIST_PATH}" \
-| awk '{printf "\"%s\" ", $0}' \
+| awk 'NF {printf "\"%s\" ", $0}' \
 | xargs -0 -I {} sh -c 'cue export {} --out yaml' \
 | tee "${PARAM_GENERATED_CONFIG_PATH}"
 
